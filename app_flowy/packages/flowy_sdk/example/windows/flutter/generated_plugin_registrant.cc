d395f93871aaa8b2119889d21ceeecc70f7475ed
appflowy <annie@appflowy.io>
2021-11-15 23:09:32 +0800

    [flutter]: enable windows platform on flowysdk

---
+++ app_flowy/packages/flowy_sdk/example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flowy_sdk/flowy_sdk_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlowySdkPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlowySdkPlugin"));
+}
